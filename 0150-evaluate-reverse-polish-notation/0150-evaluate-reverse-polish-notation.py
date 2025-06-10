4c8c09ae531c170ff253f0c7683307710b6e5d4b
Jatin Uppal <jatinuppal55@gmail.com>
2025-06-09 19:42:01 -0700

    Time: 3 ms (65.45%), Space: 19.1 MB (52.11%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -2,13 +2,17 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         for char in tokens:
-            if char != "+" and char != "-" and char != "*" and char != "/":
-                stack.append(char)
-            elif stack:
-                var1 = stack.pop()
-                var2 = stack.pop()
-                expr = str(var2) + char + str(var1)
-                result = int(eval(expr))
-                stack.append(result)
-        res = int(stack.pop())
-        return res
+            if char not in {"+", "-", "*", "/"}:
+                stack.append(int(char))
+            else:
+                b = stack.pop()
+                a = stack.pop()
+                if char == "+":
+                    stack.append(a + b)
+                elif char == "-":
+                    stack.append(a - b)
+                elif char == "*":
+                    stack.append(a * b)
+                elif char == "/":
+                    stack.append(int(a / b))
+        return stack[0]
