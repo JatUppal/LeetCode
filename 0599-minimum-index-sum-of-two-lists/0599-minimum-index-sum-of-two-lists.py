b5691daddf7848592d529fa1d294f9f45ed79683
Jatin Uppal <jatinuppal55@gmail.com>
2025-06-19 17:32:29 -0700

    Time: 7 ms (75.21%), Space: 18.1 MB (91.46%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -1,9 +1,10 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
         res = {}
+        dict1 = {val: index for index, val in enumerate(list1)}
         for i in range(len(list2)):
-            if list2[i] in list1:
-                sums = i + list1.index(list2[i])
+            if list2[i] in dict1.keys():
+                sums = i + dict1[list2[i]]
                 if sums not in res:
                     res[sums] = []
                 res[sums].append(list2[i])
