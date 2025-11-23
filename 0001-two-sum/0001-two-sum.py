1bb6bfade7200f02cce69a3579e35edadfd68068
Jatin Uppal <jatinuppal55@gmail.com>
2025-11-23 13:14:12 -0800

    Time: 0 ms (100%), Space: 18.9 MB (51.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,6 +4,6 @@ class Solution:
         for i, num in enumerate(nums):
             comp = target - num
             if comp in seen:
-                return (seen[comp], i)
+                return [seen[comp], i]
             seen[num] = i
-        return None
+        
