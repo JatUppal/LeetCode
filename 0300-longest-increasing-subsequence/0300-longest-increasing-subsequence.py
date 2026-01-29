5f6ef396b03b0ba704faff8e528b93c3ef682abc
Jatin Uppal <jatinuppal55@gmail.com>
2026-01-29 15:07:41 -0600

    Time: 12 ms (73.35%), Space: 19.3 MB (45.26%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,18 +1,25 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # tails[i] stores the smallest possible ending value
+        # of an increasing subsequence of length i + 1
         tails = []
-
+        # Iterate through each number in the array
         for x in nums:
+            # Binary search on tails to find position for x
             l, r = 0, len(tails)
             while l < r:
                 mid = (l + r) // 2
+                # If x is larger, it can extend a longer subsequence
                 if x > tails[mid]:
                     l = mid + 1
+                # Otherwise, try to replace a larger value
                 else:
                     r = mid
-            # now l is the position to place x
+            # If x extends the longest subsequence so far
             if l == len(tails):
                 tails.append(x)
+            # Otherwise, replace the tail to keep it minimal
             else:
                 tails[l] = x
+        # Length of tails equals length of LIS
         return len(tails)
