c83aed2eb6666447d9ebe3027e00cdd5290e68b7
Jatin Uppal <jatinuppal55@gmail.com>
2026-01-29 15:39:17 -0600

    Time: 0 ms (100%), Space: 19.6 MB (16.98%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,8 +1,13 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+      # Iterate through each row in the matrix
         for row in matrix:
+            # If the last element of the row is >= target,
+            # then the target could be in this row
             if row[-1] >= target:
+                # Scan through the row to check for the target
                 for col in row:
                     if col == target:
-                        return True
+                        return True  # Target found
+        # Target was not found in any row
         return False
