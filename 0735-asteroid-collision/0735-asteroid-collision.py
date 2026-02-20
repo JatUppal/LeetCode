ce3d4eea5daf1237e17a5d7f535bb2e3d59bd945
Jatin Uppal <jatinuppal55@gmail.com>
2026-02-19 21:16:21 -0600

    Time: 7 ms (59.68%), Space: 20.4 MB (27.83%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -3,13 +3,14 @@ class Solution:
         stack = []
         for a in asteroids:
             alive = True
-            while alive and a < 0 and stack and stack[-1] > 0:
-                if stack[-1] < -a:          # top smaller -> top explodes
+            while stack and alive and a < 0 and stack[-1] > 0:
+                if stack[-1] < abs(a):
                     stack.pop()
-                    continue
-                elif stack[-1] == -a:       # equal -> both explode
+                elif stack[-1] == abs(a):
                     stack.pop()
-                alive = False               # current explodes (or both did)
+                    alive = False
+                else:
+                    alive = False
             if alive:
                 stack.append(a)
         return stack
