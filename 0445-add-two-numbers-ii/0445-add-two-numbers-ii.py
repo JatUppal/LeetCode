ea088147c8a632debce9e7ed395890355f22faad
Jatin Uppal <jatinuppal55@gmail.com>
2026-02-23 12:16:54 -0600

    Time: 0 ms (100%), Space: 19.5 MB (42.39%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -13,36 +13,16 @@ class Solution:
         while l2:
             stack2.append(l2.val)
             l2 = l2.next
-        # [3,4, 2, 7]
-        # [4, 6, 5]
-        res = []
         carry = 0
-        while stack1 and stack2:
-            num1 = stack1.pop()
-            num2 = stack2.pop()
-            total = num1 + num2
-            if carry == 1:
-                total += 1
-                carry = 0
-            if total >= 10:
-                total = total % 10
-                carry = 1
-            res.append(total)
-        remain = stack1 if stack1 else stack2
-        while remain:
-            value = remain.pop()
-            if carry == 1:
-                value += 1
-                carry = 0
-            if value >= 10:
-                value = value % 10
-                carry = 1
-            res.append(value)
-        if carry == 1:
-            res.append(carry)
-        dummy = ListNode()
-        curr = dummy
-        for i in range(len(res) - 1, -1, -1):
-            curr.next = ListNode(res[i])
-            curr = curr.next
-        return dummy.next            
+        head = None
+        while stack1 or stack2 or carry:
+            a = stack1.pop() if stack1 else 0
+            b = stack2.pop() if stack2 else 0
+            total = a + b + carry
+            carry = total // 10
+            digit = total % 10
+
+            node = ListNode(digit)
+            node.next = head
+            head = node
+        return head
