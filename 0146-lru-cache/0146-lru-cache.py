8d0d62405daaf09773cf188521201a496bd4c767
Jatin Uppal <jatinuppal55@gmail.com>
2026-02-22 18:54:56 -0600

    Time: 138 ms (39.74%), Space: 78.8 MB (11.8%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -6,47 +6,45 @@ class Node:
 
 class LRUCache:
     def __init__(self, capacity: int):
-        self.size = capacity
-        self.cache = {}
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
+       self.cache = {}
+       self.size = capacity
+       self.left = Node(0, 0)
+       self.right = Node(0, 0)
+       self.left.next = self.right
+       self.right.prev = self.left
 
-    def insert(self, node):
-        temp = self.right.prev
-        self.right.prev = node
-        node.prev = temp
-        node.next = self.right
-        temp.next = node
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next = nxt
-        nxt.prev = prev
-
-    def get(self, key: int) -> int:
-        if key in self.cache.keys():
+    def get(self, key : int):
+        if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].value
-        return -1
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache.keys():
-            self.remove(self.cache[key])
-            self.cache[key] = Node(key, value)
-            self.insert(self.cache[key])
         else:
-            self.cache[key] = Node(key, value)
-            if self.size < len(self.cache):
+            return -1
+
+    def insert(self, node):
+        prv = self.right.prev
+        node.next = self.right
+        self.right.prev = node
+        prv.next = node
+        node.prev = prv
+
+    def remove(self, node):
+        nxt, prv = node.next, node.prev
+        nxt.prev = prv
+        prv.next = nxt
+
+    def put(self, key : int, value : int):
+        node = Node(key, value)
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(node)
+        else:
+            if self.size == len(self.cache):
                 del self.cache[self.left.next.key]
                 self.remove(self.left.next)
-                self.insert(self.cache[key])
+                self.insert(node)
             else:
-                self.insert(self.cache[key])
-
-
-
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+                self.insert(node)
+        self.cache[key] = node
+        
+    
