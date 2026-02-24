bde7cafde5faa79057eb03b24994d97c815e1d65
Jatin Uppal <jatinuppal55@gmail.com>
2026-02-24 14:56:16 -0600

    Time: 260 ms (31.12%), Space: 31.1 MB (11.15%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,16 +4,16 @@ class Solution:
         visited = set()
         def dfs(i, j):
             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] == "0" or (i, j) in visited:
-                return
-            elif grid[i][j] == "1":
-                visited.add((i, j))
+                return 0
+            visited.add((i, j))
             dfs(i + 1, j)
             dfs(i - 1, j)
             dfs(i, j + 1)
             dfs(i, j - 1)
+
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == "1" and (i, j) not in visited:
-                    islands += 1
                     dfs(i, j)
+                    islands += 1
         return islands
