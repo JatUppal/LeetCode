ea6a96e685fc6b0792fc66aeaf7d50aefcd7fc07
Jatin Uppal <jatinuppal55@gmail.com>
2026-02-24 22:36:51 -0600

    Time: 3 ms (77.7%), Space: 23.1 MB (40.34%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,23 +1,22 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        res = 0
         n = len(matrix)
         l = matrix[0][0]
-        h = matrix[n - 1][n-1]
-        def count_le(x):
-            c = 0
+        h = matrix[n-1][n-1]
+        def count_steps(num : int):
             r = n - 1
-            cnt = 0
+            c = 0
+            steps = 0
             while r >= 0 and c < n:
-                if matrix[r][c] <= x:
-                    cnt += r + 1
+                if matrix[r][c] <= num:
+                    steps += r + 1
                     c += 1
                 else:
                     r -= 1
-            return cnt
+            return steps
         while l < h:
             mid = (l + h) // 2
-            if count_le(mid) < k:
+            if count_steps(mid) < k:
                 l = mid + 1
             else:
                 h = mid
