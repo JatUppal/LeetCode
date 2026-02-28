5b9f3d75739cef70d924eb507e2e684be3dd32b9
Jatin Uppal <jatinuppal55@gmail.com>
2026-02-28 12:27:29 -0600

    Time: 124 ms (57.59%), Space: 78.8 MB (21.04%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -5,10 +5,10 @@ class Node:
         self.next = self.prev = None
 
 class LRUCache:
-    def __init__(self, capacity: int):
+    def __init__(self, capacity : int):
         self.size = capacity
         self.cache = {}
-        self.left, self.right = Node(0,0), Node(0,0)
+        self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next = self.right
         self.right.prev = self.left
     
@@ -19,25 +19,24 @@ class LRUCache:
             return self.cache[key].value
         else:
             return -1
+    def insert(self, node):
+        prv = self.right.prev
+        prv.next = node
+        self.right.prev = node
+        node.next = self.right
+        node.prev = prv
 
     def remove(self, node):
         nxt, prv = node.next, node.prev
         nxt.prev = prv
         prv.next = nxt
 
-    def insert(self, node):
-        temp = self.right.prev
-        temp.next = node
-        node.next = self.right
-        node.prev = temp
-        self.right.prev = node
-
     def put(self, key : int, value : int):
         node = Node(key, value)
         if key in self.cache:
             self.remove(self.cache[key])
-            self.cache[key] = node
             self.insert(node)
+            self.cache[key] = node
         else:
             if self.size == len(self.cache):
                 del self.cache[self.left.next.key]
