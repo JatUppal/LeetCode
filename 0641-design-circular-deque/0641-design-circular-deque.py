d15a36cd19477948e31b01c1be6bed596109c5dd
Jatin Uppal <jatinuppal55@gmail.com>
2026-03-22 14:29:42 -0500

    Time: 7 ms (62.06%), Space: 20.1 MB (77.92%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.py
+++ 0641-design-circular-deque/0641-design-circular-deque.py
@@ -1,52 +1,69 @@
 class MyCircularDeque:
 
     def __init__(self, k: int):
-        self.deque = []
+        self.deque = [0] * k
         self.size = k
+        self.front = 0
+        self.rear = -1
+        self.count = 0
 
     def insertFront(self, value: int) -> bool:
-        if len(self.deque) == self.size:
+        if self.isFull():
             return False
-        self.deque.insert(0, value)
+        self.front = (self.front - 1 + self.size) % self.size
+        self.deque[self.front] = value
+        if self.count == 0:
+            self.rear = self.front
+        self.count += 1
         return True
 
     def insertLast(self, value: int) -> bool:
-        if len(self.deque) == self.size:
+        if self.isFull():
             return False
-        self.deque.append(value)
+        self.rear = (self.rear + 1) % self.size
+        self.deque[self.rear] = value
+        if self.count == 0:
+            self.front = self.rear
+        self.count += 1
         return True
 
     def deleteFront(self) -> bool:
-        if self.deque:
-            self.deque.pop(0)
-            return True
-        return False
-
-    def deleteLast(self) -> bool:
-        if self.deque:
-            self.deque.pop()
-            return True
-        return False
-
-    def getFront(self) -> int:
-        if self.deque:
-            return self.deque[0]
-        return -1
-
-    def getRear(self) -> int:
-        if self.deque:
-            return self.deque[-1]
-        return -1
-
-    def isEmpty(self) -> bool:
-        if self.deque:
+        if self.isEmpty():
             return False
+        self.front = (self.front + 1) % self.size
+        self.count -= 1
+
+        if self.count == 0:
+            self.front = 0
+            self.rear = -1
         return True
 
+    def deleteLast(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.rear = (self.rear - 1 + self.size) % self.size
+        self.count -= 1
+        
+        if self.count == 0:
+            self.front = 0
+            self.rear = -1
+        return True
+
+    def getFront(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.deque[self.front]
+
+    def getRear(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.deque[self.rear]
+
+    def isEmpty(self) -> bool:
+        return self.count == 0
+
     def isFull(self) -> bool:
-        if len(self.deque) == self.size:
-            return True
-        return False
+        return self.count == self.size
 
 
 # Your MyCircularDeque object will be instantiated and called as such:
